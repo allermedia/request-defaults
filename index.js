04c0860b9b5ea41b422685d516e388da08f94586
Mikko Tikkanen <mikko.tikkanen@gmail.com>
2015-02-27 11:12:32 +0200

    Removed default timeout

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
  * 
  */
 /* jshint node: true */
-module.exports = require('request').defaults({
-  timeout : 10*1000
-});
+module.exports = require('request').defaults({});
 
 module.exports.globalDefaults = function(options) {
   module.exports = require('request').defaults(options);
